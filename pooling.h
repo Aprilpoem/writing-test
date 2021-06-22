dca21542cbe796d09b62dd9ed1173d52351333b8
WuLongShi <courage852@sina.cn>
2021-06-22 13:53:22 +0800

    Update pooling.h

--- pooling.h
+++ pooling.h
@@ -4,12 +4,14 @@
 #ifndef INTEL_TASK_POOLING_H
 #define INTEL_TASK_POOLING_H
 #include "data.h"
-
+#include <algorithm>
+#include <omp.h>
 class PoolingLayer{
 public:
     Data naive_maxpool2d(Data& input,int kernel_size,int *stride, int *padding);
     Data naive_avgpool2d(Data& input,int kernel_size,int *stride, int *padding);
     Data maxpool2d(Data& input,int kernel_size,int *stride, int *padding);
+    Data maxpool2d1(Data& input,int kernel_size,int *stride, int *padding);
     Data avgpool2d(Data& input,int kernel_size,int *stride, int *padding);
 };
 #endif //INTEL_TASK_POOLING_H
